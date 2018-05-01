a95f1fc90a1d88be87e5cb0065600cd5cda40dc5
Jody Vanden <jody.vanden@gmail.com>
2018-05-02 06:57:11 +1000

    Amin field added to user model and seed first admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+unless User.exists?(email: "admin@g.com")
+  User.create(email: "admin@g.com", password: "123456", admin: true )
+end
+
+unless User.exists?(email: "viewer@g.com")
+  User.create(email: "viewer@g.com", password: "123456")
+end
+
+["Sublime Text 3", "Internet Explorer"].each do |name|
+  unless Project.exists?(name: name)
+    Project.create!(name: name, description: "A sample project about #{name}"
+     )
+end end
